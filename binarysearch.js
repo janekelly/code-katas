49163bcb2bccf326a0d976721ab52ca8026a4791
Jane Kelly <jane.kelly@pancentric.com>
2015-08-25 16:49:39 +0100

    updated so that the original array doesn't get altered

--- binarysearch.js
+++ binarysearch.js
@@ -1,24 +1,32 @@
 (function () {
 	var arrayToSearch = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40],
-		requiredNumber = 10;
+		requiredNumber = 31;
 
 	// Split the array in two
 	// if the last item is greater than the number
 
 	function searchArray(array) {
-		if (array.length > 1) {
-			var firstPart = array.splice(0, array.length/2),
-				secondPart = array.splice(array.length/2, array.length);
+
+		var firstPart = array.slice(),
+			secondPart = array.slice(),
+			arrayLength = array.length;
+
+		if (arrayLength > 1) {
+			firstPart = firstPart.splice(0, Math.ceil(arrayLength/2));
+			secondPart = secondPart.splice(Math.ceil(arrayLength/2), arrayLength-1);
+
+
+			console.log("The first half is " + firstPart);
+			console.log("The second half is " + secondPart);
 
 			if (firstPart.indexOf(requiredNumber) > -1) {
-				// it must be in this array
-				console.log(firstPart);
+				// it must be in this numbers
 				searchArray(firstPart);
 			} else if (secondPart.indexOf(requiredNumber) > -1) {
-				// it must be in this array
+				// it must be in this numbers
 				searchArray(secondPart);
 			} else {
-				console.log('We either found the number or something went wrong');
+				console.log('something went wrong');
 			}
 		} else {
 			console.log(array);
